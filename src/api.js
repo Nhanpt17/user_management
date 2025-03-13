b04c6219e2f3b8379a752837494ab3f56c32b66d
Pham Thanh Nhan <pnhan1792003@gmail.com>
2025-03-13 21:02:57 +0700

    add urlapi

--- src/api.js
+++ src/api.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
+// const API = axios.create({
+//   baseURL: "http://127.0.0.1:8000/api",
+//   headers: { "Content-Type": "application/json" },
+// });
+
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL:
+    window.location.hostname === "localhost"
+      ? "http://127.0.0.1:8000/api" // URL local
+      : "https://user-management-be-appk.onrender.com/api", // URL trên Render
   headers: { "Content-Type": "application/json" },
 });
 
